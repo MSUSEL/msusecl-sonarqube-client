e63a02e425ff967261873a6cf93e7bbaac99e6ec
Ryan Cummings <ryan.cummings1@montana.edu>
2025-08-12 15:23:29 -0600

    some template code

---
+++ src/main/java/api/projects/Calls.java
@@ -0,0 +1,17 @@
+package api.projects;
+
+/**
+ * Hit SonarQube's api/projects/* endpoints
+ */
+public class Calls {
+    public void bulk_delete() {}
+    public void create() {}
+    public void delete() {}
+    public void export_findings() {}
+    public void get_contains_ai_code() {}
+    public void license_usage() {}
+    public void search() {}
+    public void set_contains_ai_code() {}
+    public void update_key() {}
+    public void update_visibility() {}
+}
