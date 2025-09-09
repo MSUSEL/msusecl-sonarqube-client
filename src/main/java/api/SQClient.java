83ed6cc6d42354ce9472732293c80b674a0a4948
Ryan Cummings <rcummings98@gmail.com>
2025-09-09 16:22:09 -0600

    Include factory class

--- src/main/java/api/SQClient.java
+++ src/main/java/api/SQClient.java
@@ -1,19 +1,40 @@
 package api;
 
+import api.authentication.AuthenticationCalls;
+import api.hotspots.HotspotsCalls;
+import api.issues.IssuesCalls;
 import api.projects.ProjectsCalls;
 
 import java.net.http.HttpClient;
 
+/**
+ * Factory class allowing end user to instantiate whichever calls class(es) they require
+ * All but the AuthenticationCalls, which deals only with login/password, have overloaded constructors
+ * allowing the user to use either token-based or login/password-based authentication
+ */
 public class SQClient {
-    private SQClient(HttpClient httpClient, String baseUrl, String token) {}
 
-    public static SQClient newSQClient(HttpClient httpClient, String baseUrl, String token) {
-        return new SQClient(httpClient, baseUrl, token);
+    public static ProjectsCalls newProjectsCalls(HttpClient httpClient, String baseUrl) {
+        return new ProjectsCalls(httpClient, baseUrl);
     }
 
-    public ProjectsCalls projects(HttpClient httpClient, String baseUrl, String token) {
-        return new ProjectsCalls(httpClient, baseUrl, token);
+    public static AuthenticationCalls newAuthenticationCalls(HttpClient httpClient, String baseUrl) {
+        return new AuthenticationCalls(httpClient, baseUrl);
     }
 
-    public
+    public static HotspotsCalls newHotspotsCalls(HttpClient httpClient, String baseUrl, String token) {
+        return new HotspotsCalls(httpClient, baseUrl, token);
+    }
+
+    public static HotspotsCalls newHotspotsCalls(HttpClient httpClient, String baseUrl) {
+        return new HotspotsCalls(httpClient, baseUrl);
+    }
+
+    public static IssuesCalls newIssuesCalls(HttpClient httpClient, String baseUrl, String token) {
+        return new IssuesCalls(httpClient, baseUrl, token);
+    }
+
+    public static IssuesCalls newIssuesCalls(HttpClient httpClient, String baseUrl) {
+        return new IssuesCalls(httpClient, baseUrl);
+    }
 }
